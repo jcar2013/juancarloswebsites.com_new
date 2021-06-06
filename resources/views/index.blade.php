d034a054b2ebbb1788b73c278f786e1312f47bb6
carlos <zamoranocarlos13@gmail.com>
2021-06-06 14:59:56 -0700

    centered blog post section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,22 +15,22 @@
     </div>
 </div>
 
-<div>
+<div class="blog-post-section">
     <div class="row">
-        
-        <div class="col-md-6">
+
+        <div class="col-md-6 blog-col-img">
             <img src="{{url('/images/blog-pic-1.jpg')}}" width="500" alt="">
         </div>
-        
-        <div class="col-md-6">
+
+        <div class="col-md-6 blog-col-post">
             <h2>Struggling to be a better web developer?</h2>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             </p>
             <p>
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
             </p>
-            
+
             <a href="">Find Out More</a>
         </div>
     </div>
