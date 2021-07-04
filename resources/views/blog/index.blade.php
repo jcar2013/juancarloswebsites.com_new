57ccf66f1a2904a7a402dc41995d800cdc67bbdf
carlos <zamoranocarlos13@gmail.com>
2021-07-03 23:12:43 -0700

    added database migration

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,13 +7,14 @@
             Blog Posts
         </h1>
     </div>
+    @foreach ($posts as $post)
     <div id="blog-posts" class="row">
         <div class="col-12 col-lg-6 blog-col" id="col-blog-img">
             <img src="{{ url('/images/blog-pic-1.jpg') }}" alt="">
         </div>
         <div class="col-12 col-lg-6 blog-col" id="col-blog-post">
             <h2>
-                Learn how to write Laravel Code
+                {{$post->title}}
             </h2>
 
             <span>
@@ -21,14 +22,13 @@
             </span>
 
             <p>
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-                took a galley of type and scrambled it to make a type specimen book. It has survived not only five
-                centuries, but also the leap into electronic typesetting,
+                {{$post->description}}
             </p>
 
-            <a href="" class="btn btn-outline-secondary btn-lg" role="button">Read learn-more-btn</a>
+            <a href="/blog/{{$post->slug}}" class="btn btn-outline-secondary btn-lg" role="button">Read learn-more-btn</a>
 
         </div>
     </div>
+    @endforeach
 </div>
 @endsection
