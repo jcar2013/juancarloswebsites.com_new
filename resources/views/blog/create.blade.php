40ebc7a7d548d3238778e6fbf191cd00a9844348
carlos <zamoranocarlos13@gmail.com>
2021-07-05 00:14:37 -0700

    added create post form

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -2,14 +2,32 @@ Create
 @extends('layouts.app')
 
 @section('content')
-    <div id="blog" class="container-fluid">
-        <div>
-            <h1>
-                Create Posts
-            </h1>
-        </div>
-
-
-
+<div id="blog" class="container-fluid">
+    <div>
+        <h1>
+            Create Posts
+        </h1>
     </div>
+
+    <div>
+        <form action="/blog" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="text" name="title" placeholder="Title..." class="">
+            <textarea name="description" id="" cols="30" placeholder="Description..." rows="10"></textarea>
+            <div>
+                <label for="">
+                    <span>
+                        Select a file
+                    </span>
+                    <input type="file" name="image" class="hidden">
+                </label>
+            </div>
+
+            <button type="submit" class="">
+                Submit Post
+            </button>
+        </form>
+    </div>
+
+</div>
 @endsection
