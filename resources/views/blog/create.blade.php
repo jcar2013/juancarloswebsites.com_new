ef571ecbaed27dac195a9b3e8c47feef2eb7fd90
carlos <zamoranocarlos13@gmail.com>
2021-07-04 19:40:05 -0700

    added create post page

---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,15 @@
+Create
+@extends('layouts.app')
+
+@section('content')
+    <div id="blog" class="container-fluid">
+        <div>
+            <h1>
+                Create Posts
+            </h1>
+        </div>
+
+
+
+    </div>
+@endsection
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,35 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="blog" class="container-fluid">
-    <div>
-        <h1>
-            Blog Posts
-        </h1>
-    </div>
-    @foreach ($posts as $post)
-    <div id="blog-posts" class="row">
-        <div class="col-12 col-lg-6 blog-col" id="col-blog-img">
-            <img src="{{ url('/images/blog-pic-1.jpg') }}" alt="">
+    <div id="blog" class="container-fluid">
+        <div>
+            <h1>
+                Blog Posts
+            </h1>
         </div>
-        <div class="col-12 col-lg-6 blog-col" id="col-blog-post">
-            <h2>
-                {{$post->title}}
-            </h2>
 
-                <span>
-                    By <span>Code with Carlos</span>, 1 day ago
-                </span>
-
-            <p>
-                {{$post->description}}
-            </p>
-
-            <a href="/blog/{{$post->slug}}" class="btn btn-outline-secondary btn-lg" role="button">Read learn-more-btn</a>
-
+        @if (Auth::check())
+            <div class="pt-15 w-4/5 m-auto">
+                <a href="/blog/create" class="">Create Post</a>
             </div>
-        </div>
+        @endif
+
+        @foreach ($posts as $post)
+            <div id="blog-posts" class="row">
+                <div class="col-12 col-lg-6 blog-col" id="col-blog-img">
+                    <img src="{{ url('/images/blog-pic-1.jpg') }}" alt="">
+                </div>
+                <div class="col-12 col-lg-6 blog-col" id="col-blog-post">
+                    <h2>
+                        {{ $post->title }}
+                    </h2>
+
+                    <span>
+                        By <span id="blog-author">{{ $post->user->name }}</span>, Created on
+                        {{ date('jS M Y', strtotime($post->updated_at)) }}
+                    </span>
+
+                    <p>
+                        {{ $post->description }}
+                    </p>
+
+                    <a href="/blog/{{ $post->slug }}" class="btn btn-outline-secondary btn-lg" role="button">Read
+                        learn-more-btn</a>
+
+                </div>
+            </div>
     </div>
     @endforeach
-</div>
+    </div>
 @endsection
