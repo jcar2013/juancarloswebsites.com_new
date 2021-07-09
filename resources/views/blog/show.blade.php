866e2703a5768a375889d0ab54569c4e2b7501c5
carlos <zamoranocarlos13@gmail.com>
2021-07-08 23:33:03 -0700

    adding show post page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,43 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="blog" class="container-fluid">
-        <div>
-            <h1>
-                {{ $post->title }}
-            </h1>
-        </div>
-
-        @if ($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>
-                            {{ $error }}
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <div>
-            <form action="/blog" method="POST" enctype="multipart/form-data">
-                @csrf
-                <input type="text" name="title" placeholder="Title..." class="">
-                <textarea name="description" id="" cols="30" placeholder="Description..." rows="10"></textarea>
-                <div>
-                    <label for="">
-                        <span>
-                            Select a file
-                        </span>
-                        <input type="file" name="image" class="hidden">
-                    </label>
-                </div>
-
-                <button type="submit" class="">
-                    Submit Post
-                </button>
-            </form>
-        </div>
-
+<div id="show" class="container-fluid">
+    <div>
+        <h1>
+            {{ $post->title }}
+        </h1>
     </div>
+
+    <div>
+        <span>
+            By <span>{{$post->user->name }}</span>, Created on
+            {{ date('jS M Y', strtotime($post->updated_at)) }}
+        </span>
+    </div>
+
+    <p>
+        {{$post->description}}
+    </p>
+
+</div>
 @endsection
