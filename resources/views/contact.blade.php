62a9098f9d397b86da28cec7a790724fd625add0
carlos <zamoranocarlos13@gmail.com>
2021-12-28 02:18:53 -0800

    added container-fluid to contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,94 +2,99 @@
 
 
 @section('content')
-<div id="contact-me" class="">
+    <div id="contact-me" class="container-fluid">
 
-    <!-- Success message -->
-    @if(Session::has('success'))
-    <div class="alert alert-success">
-        {{Session::get('success')}}
-    </div>
-    @endif
+        <!-- Success message -->
+        @if (Session::has('success'))
+            <div class="alert alert-success">
+                {{ Session::get('success') }}
+            </div>
+        @endif
 
-    <div class="row">
-        <div class="col-md-6">
-            <div id="contact-info-col">
+        <div class="row">
+            <div class="col-md-6">
+                <div id="contact-info-col">
 
-                <h1 id="title-text">
-                    Contact Us
-                </h1>
+                    <h1 id="title-text">
+                        Contact Us
+                    </h1>
 
+                </div>
+            </div>
+            <div class="col-md-6">
+
+                <div id="contact-form-col">
+
+
+                    <form action="" id="contact-me-form" method="post" action="{{ route('contact.store') }}">
+
+                        @csrf
+
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" class="form-control {{ $errors->has('name') ? 'error' : '' }}" name="name"
+                                id="name">
+
+                            <!-- Error -->
+                            @if ($errors->has('name'))
+                                <div class="error">
+                                    {{ $errors->first('name') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control {{ $errors->has('email') ? 'error' : '' }}"
+                                name="email" id="email">
+
+                            @if ($errors->has('email'))
+                                <div class="error">
+                                    {{ $errors->first('email') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label>Phone</label>
+                            <input type="text" class="form-control {{ $errors->has('phone') ? 'error' : '' }}"
+                                name="phone" id="phone">
+
+                            @if ($errors->has('phone'))
+                                <div class="error">
+                                    {{ $errors->first('phone') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label>Subject</label>
+                            <input type="text" class="form-control {{ $errors->has('subject') ? 'error' : '' }}"
+                                name="subject" id="subject">
+
+                            @if ($errors->has('subject'))
+                                <div class="error">
+                                    {{ $errors->first('subject') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label>Message</label>
+                            <textarea class="form-control {{ $errors->has('message') ? 'error' : '' }}" name="message"
+                                id="message" rows="4"></textarea>
+
+                            @if ($errors->has('message'))
+                                <div class="error">
+                                    {{ $errors->first('message') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <input type="submit" name="send" value="Submit" class="btn btn-dark btn-block">
+                    </form>
+                </div>
             </div>
         </div>
-        <div class="col-md-6">
-
-            <div id="contact-form-col">
-
-
-                <form action="" id="contact-me-form" method="post" action="{{ route('contact.store') }}">
-
-                    @csrf
-
-                    <div class="form-group">
-                        <label>Name</label>
-                        <input type="text" class="form-control {{ $errors->has('name') ? 'error' : '' }}" name="name" id="name">
-
-                        <!-- Error -->
-                        @if ($errors->has('name'))
-                        <div class="error">
-                            {{ $errors->first('name') }}
-                        </div>
-                        @endif
-                    </div>
-
-                    <div class="form-group">
-                        <label>Email</label>
-                        <input type="email" class="form-control {{ $errors->has('email') ? 'error' : '' }}" name="email" id="email">
-
-                        @if ($errors->has('email'))
-                        <div class="error">
-                            {{ $errors->first('email') }}
-                        </div>
-                        @endif
-                    </div>
-
-                    <div class="form-group">
-                        <label>Phone</label>
-                        <input type="text" class="form-control {{ $errors->has('phone') ? 'error' : '' }}" name="phone" id="phone">
-
-                        @if ($errors->has('phone'))
-                        <div class="error">
-                            {{ $errors->first('phone') }}
-                        </div>
-                        @endif
-                    </div>
-
-                    <div class="form-group">
-                        <label>Subject</label>
-                        <input type="text" class="form-control {{ $errors->has('subject') ? 'error' : '' }}" name="subject" id="subject">
-
-                        @if ($errors->has('subject'))
-                        <div class="error">
-                            {{ $errors->first('subject') }}
-                        </div>
-                        @endif
-                    </div>
-
-                    <div class="form-group">
-                        <label>Message</label>
-                        <textarea class="form-control {{ $errors->has('message') ? 'error' : '' }}" name="message" id="message" rows="4"></textarea>
-
-                        @if ($errors->has('message'))
-                        <div class="error">
-                            {{ $errors->first('message') }}
-                        </div>
-                        @endif
-                    </div>
-
-                    <input type="submit" name="send" value="Submit" class="btn btn-dark btn-block">
-                </form>
-            </div>
-        </div>
     </div>
-</div>
 @endsection
